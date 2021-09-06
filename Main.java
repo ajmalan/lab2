55f3040a98de49a785ae0808c314f385a5b3ea3b
ajmalan <89461079+ajmalan@users.noreply.github.com>
2021-09-06 19:23:08 +0000

    autoformat

--- Main.java
+++ Main.java
@@ -2,14 +2,14 @@ class Main {
   public static void main(String[] args) {
     System.out.println("M&M Color Counts");
     // Declaring variables
-    int total = 55*11;
-    double blue = total*.24 ;
-    double brown = total*.13;
-    double green = total*.16;
-    double orange = total*.2;
-    double red = total*.13;
-    double yellow = total*.14;
-   // Print Statements for M&M Calculations
+    int total = 55 * 11;
+    double blue = total * .24;
+    double brown = total * .13;
+    double green = total * .16;
+    double orange = total * .2;
+    double red = total * .13;
+    double yellow = total * .14;
+    // Print Statements for M&M Calculations
     System.out.println("blue: " + blue);
     System.out.println("brown: " + brown);
     System.out.println("green: " + green);
@@ -19,12 +19,12 @@ class Main {
     // Sum Statements
     double sum = blue + brown + green + orange + red + yellow;
     System.out.println("Sum: " + sum);
-    //Comparison Booleans
-    if ( blue < brown && red > orange)
-      System.out.println ("Blue under Brown and Red over Orange");
-if ( brown >= green)
-     System.out.println ("Brown is greater than or equal to Green");
-if ( sum == total)
-     System.out.println ("Numbers match");
+    // Comparison Booleans
+    if (blue < brown && red > orange)
+      System.out.println("Blue under Brown and Red over Orange");
+    if (brown >= green)
+      System.out.println("Brown is greater than or equal to Green");
+    if (sum == total)
+      System.out.println("Numbers match");
   }
 }
